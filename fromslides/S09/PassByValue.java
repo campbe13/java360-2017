008aa2621ad9809638f0f1703d941ed7e236c2ff
P.M.Campbell <pcampbell.edu@gmail.com>
2016-02-21 23:19:02 +0000

    source for slides 9

--- fromslides/S09/PassByValue.java
+++ fromslides/S09/PassByValue.java
@@ -1,4 +1,4 @@
-//package fromslides.S08;
+//package fromslides.S09;
 import java.util.Scanner;
 /**
  * This class illustrates exercises from the slide deck for the course
@@ -6,110 +6,33 @@ import java.util.Scanner;
  * @author PMCampbell
  * @version today
  **/
-public class SimpleMethods2
+public class PassByValue
 {
- 
     public static void main(String[] args)
     {
-    int num1, num2;
+    int number = 99;
     
-    message("Teachers");
-
-    num1 = readInt();
-    num2 = readInt();
-    
-    showMinMax(num1, num2);
-    
-    message("P. M. Campbell");
-    } // main()
-    
-    /** 
-     * Compares the two parameters 
-     * returns the smallest of the two
-     * 
-     * @param a integer
-     * @param b integer
-     * @return smallest of a and b integer
-     * @author PMCampbell
-     * @version today
-     * */
-    
-    public static int min(int a, int b) {
-        int small;
-        if ( a > b)
-           small = b;
-        else 
-           small = a;
-           
-        return small;
-    }  // min()
-    
-    /** 
-     * Compares the two parameters 
-     * returns the largest of the two
-     * 
-     * @param a integer
-     * @param b integer
-     * @return largest of a and b
-     * @author PMCampbell
-     * @version today
-     * */
-    
-    public static int max(int a, int b) {
-        int big;
-        if ( a > b)
-           big = a;
-        else 
-           big = b;
-           
-        return big;
-    }  // max()
-    
-    /** 
-     * Displays the numbers given as params
-     * Determines the smallest of the two and  displays it
-     * Determines the largest of the two and displays it
-     * 
-     * @param num1 int
-     * @param num2 int
-     * @author PMCampbell
-     * @version today
-     * */
-    
-    
-    public static void showMinMax(int num1, int num2) {
-        System.out.printf("Given %d and %d\n", num1, num2);
-        System.out.println("Smallest is "+ min(num1,num2));
-        System.out.println("Biggest is "+ max(num1,num2));
-    }  // showMinMax()
-    
-    /** 
-     * Displays a prompt, reads in an int 
-     * 
-     * @return int
-     * @author PMCampbell
-     * @version today
-     * */
-    
-    public static int readInt() {
-        int num;
-        Scanner keyboard = new Scanner(System.in);
-        
-        System.out.print("Enter an integer: ");
-        num =  keyboard.nextInt();
-        return num;
-        
-    }  // readInt()
-    
-    /** 
-     * Displays a message
-     * 
-     * @param programmer name String
-     * @author PMCampbell
-     * @version today
-     * */
-
-    public static void message(String programmer) {
-        System.out.println("Brought to you by Dawson College "+programmer);
-    }  // message()
-} // SimpleMethods2
+    System.out.println("Number is "+number);
+ 
+    changeMe(number); // Call changeMe
+     
+      // Display the value in number again
+      System.out.println("number is " + number); // Display the value in number
+   } // main()
+   
+   /**
+    * The changeMe method accepts an argument and then
+    * changes the value of the parameter
+    * what really happens?
+    * 
+    * @param number int
+      @author PMC
+      @version today
+   */
+   public static void changeMe(int myValue)
+   {
+      System.out.println("I am changing the value.");
+      myValue = 0;
+      System.out.println("Now the value is " + myValue);
+   } //changeMe()
+} // PassByValue
