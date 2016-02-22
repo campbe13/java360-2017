ace98b67dd623918fbb445859a1af4b72f914400
P.M.Campbell <pcampbell.edu@gmail.com>
2016-02-22 18:59:19 +0000

    countdown methods

---
+++ fromslides/S09/CountDownMethods.java
@@ -0,0 +1,48 @@
+//package fromslides.S08;
+import java.util.Scanner;
+
+/**
+ * This class illustrates exercises from the slide deck for the course
+ * 360-420-DW Intro to Java
+ * @author PMCampbell
+ * @version today
+ **/
+public class CountDownMethods {
+   public static void main(String[] args) {
+
+         int start;
+         Scanner kb = new Scanner(System.in);
+
+         // Get the starting point
+         System.out.print("Count down from: ");
+         start = kb.nextInt();
+         countIt(start);
+         
+         System.out.println("End 1st countdown");
+         
+         // Get the starting point
+         System.out.print("Count down from: ");
+         start = kb.nextInt();
+         countIt(start);
+         
+         System.out.println("Blastoff!");
+
+      } // main()
+   /**
+   * This method displays the parameter and decrements it by one
+   * 360-420-DW Intro to Java
+   * @author PMCampbell
+   * @version today
+   * @param num int
+   * @return void
+   **/
+   public static void countIt(int num) {
+
+         while (num >= 0) {
+
+            System.out.println(num);
+            num--;
+
+         }
+      } // countIt()
+} // CountDown
